75a4e88d5dc9440c191fe4c8dae794d481890266
StrelitziaReginae <qr896035@163.com>
2024-02-17 02:27:54 +0800

    permission配置

--- app/src/main/java/com/artillery/androidmusic/LauncherActivity.java
+++ app/src/main/java/com/artillery/androidmusic/LauncherActivity.java
@@ -2,11 +2,18 @@ package com.artillery.androidmusic;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+
 import com.artillery.music.BR;
 import com.artillery.music.R;
 import com.artillery.music.databinding.ActivityLauncherBinding;
 import com.artillery.musicbase.base.BaseActivity;
 import com.artillery.musicmain.ui.MusicMainFragment;
+import com.hjq.permissions.OnPermissionCallback;
+import com.hjq.permissions.Permission;
+import com.hjq.permissions.XXPermissions;
+
+import java.util.List;
 
 /**
  * @author ArtilleryOrchid
@@ -24,7 +31,17 @@ public class LauncherActivity extends BaseActivity<ActivityLauncherBinding, Laun
 
     @Override
     public void initData() {
-        startContainerActivity(MusicMainFragment.class.getCanonicalName());
-        finish();
+        XXPermissions.with(this)
+                .permission(Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_MEDIA_IMAGES, Permission.READ_MEDIA_VIDEO, Permission.READ_MEDIA_AUDIO)
+                .request(new OnPermissionCallback() {
+                    @Override
+                    public void onGranted(@NonNull List<String> permissions, boolean allGranted) {
+                        if (!allGranted) {
+                            return;
+                        }
+                        startContainerActivity(MusicMainFragment.class.getCanonicalName());
+                        finish();
+                    }
+                });
     }
 }
