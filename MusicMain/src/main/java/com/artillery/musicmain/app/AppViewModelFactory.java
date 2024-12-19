4d24efaf53036f88b72c2192b6a7182bca9b4b86
heyongqing <v-heyongqing@xiaomi.com>
2024-12-19 15:52:35 +0800

    [improve]弹窗播放页面

--- MusicMain/src/main/java/com/artillery/musicmain/app/AppViewModelFactory.java
+++ MusicMain/src/main/java/com/artillery/musicmain/app/AppViewModelFactory.java
@@ -8,7 +8,6 @@ import androidx.lifecycle.ViewModel;
 
 import com.artillery.musicbase.base.ViewModelFactory;
 import com.artillery.musicmain.data.MusicRepository;
-import com.artillery.musicmain.viewmodel.MusicDialogViewModel;
 import com.artillery.musicmain.viewmodel.MusicMainViewModel;
 import com.artillery.musicmain.viewmodel.MusicPlayViewModel;
 
@@ -50,8 +49,6 @@ public class AppViewModelFactory extends ViewModelFactory {
             return (T) new MusicPlayViewModel(mApplication, mRepository);
         } else if (modelClass.isAssignableFrom(MusicMainViewModel.class)) {
             return (T) new MusicMainViewModel(mApplication, mRepository);
-        } else if (modelClass.isAssignableFrom(MusicDialogViewModel.class)) {
-            return (T) new MusicDialogViewModel(mApplication);
         }
         throw new IllegalArgumentException("Unknown ViewModel class: " + modelClass.getName());
     }
